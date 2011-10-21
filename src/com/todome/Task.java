f88f10cf80c5d4fe31cbc4d5e5ac54e57e9a119d
Christopher Baines <cbaines8@gmail.com>
2011-10-21 16:33:14 +0100

    Fixed the serialisation of a task.

--- src/com/todome/Task.java
+++ src/com/todome/Task.java
@@ -5,48 +5,101 @@ import java.util.ArrayList;
 
 import android.text.format.Time;
 
-public class Task implements Serializable, Comparable<Task> {	
+public class Task implements Serializable, Comparable<Task> {
 	private String name;
 	private String notes;
 	private String postcode;
 	private ArrayList<String> types;
 	private Time alarmTime;
-	
-	private boolean complete; 
+
+	private int second;
+	private int minute;
+	private int hour;
+	private int monthDay;
+	private int month;
+	private int year;
+
+	private boolean complete;
 	private int rating;
-	
-	public Task(String name, String notes, String postcode, int rating)
-	{
+
+	public Task(String name, String notes, String postcode, int rating) {
 		this.name = name;
 		this.notes = notes;
 		this.postcode = postcode;
 		this.rating = rating;
 		this.complete = false;
 	}
-	
-	public String toString()
-	{
+
+	public String toString() {
 		return name;
 	}
-	
-	public String getName() { return name; }
-	public void setName(String name) { this.name = name; }
-	public String getNotes() { return notes; }
-	public void setNotes(String notes) { this.notes = notes; }
-	public String getPostcode() { return postcode; }
-	public void setPostcode(String postcode) { this.postcode = postcode; }
-	public ArrayList<String> getTypes() {	// Returns an empty array for completed tasks
-		if (!complete) { return types; }
-		else { return new ArrayList<String>(); }
-	}
-	public void setTypes(ArrayList<String> types) { this.types = types; }
-	public boolean isComplete() { return complete; }
-	public void setComplete(boolean complete) { this.complete = complete; }
-	public int getRating() { return rating; }
-	public void setRating(int rating) { this.rating = rating; }
-	public Time getAlarmTime() {return alarmTime;}
 
-	public void setAlarmTime(Time alarmTime) {this.alarmTime = alarmTime;}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public ArrayList<String> getTypes() { // Returns an empty array for completed tasks
+		if (!complete) {
+			return types;
+		} else {
+			return new ArrayList<String>();
+		}
+	}
+
+	public void setTypes(ArrayList<String> types) {
+		this.types = types;
+	}
+
+	public boolean isComplete() {
+		return complete;
+	}
+
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public Time getAlarmTime() {
+		Time time = new Time();
+		time.set(second, minute, hour, monthDay, month, year);
+		return time;
+	}
+
+	public void setAlarmTime(Time alarmTime) {
+		second = alarmTime.second;
+		minute = alarmTime.minute;
+		hour = alarmTime.hour;
+		monthDay = alarmTime.monthDay;
+		month = alarmTime.month;
+		year = alarmTime.year;
+	}
 
 	@Override
 	public int compareTo(Task another) {
