525d49b84b804c734899b6ea5f1d6650c47fa4b9
Harry Cutts <eternal.linux@gmail.com>
2011-10-23 18:31:09 +0100

    Commented the custom location views out of new_task_dialog.xml, as well as the corresponding lines (164, 174, 217, 221) of TaskActivity.java.

--- src/com/todome/TaskActivity.java
+++ src/com/todome/TaskActivity.java
@@ -161,7 +161,7 @@ public class TaskActivity extends Activity {
 		EditText taskNameEntry = (EditText) dialog.findViewById(R.id.taskNameEntry);
 		RatingBar ratingEntry = (RatingBar) dialog.findViewById(R.id.ratingEntry);
 		EditText notesEntry = (EditText) dialog.findViewById(R.id.notesEntry);
-		EditText postcodeEntry = (EditText) dialog.findViewById(R.id.postcodeEntry);
+		//EditText postcodeEntry = (EditText) dialog.findViewById(R.id.postcodeEntry);
 		TimePicker timeEntry = (TimePicker) dialog.findViewById(R.id.timeEntry);
 
 		if (updatingTask) {
@@ -171,7 +171,7 @@ public class TaskActivity extends Activity {
 			taskNameEntry.setText(thisTask.getName());
 			ratingEntry.setRating(thisTask.getRating());
 			notesEntry.setText(thisTask.getNotes());
-			postcodeEntry.setText(thisTask.getPostcode());
+			//postcodeEntry.setText(thisTask.getPostcode());
 			if (thisTask.getAlarmTime() != null) {
 				Log.i(TAG, "AlarmTime hour " + thisTask.getAlarmTime().hour + " min " + thisTask.getAlarmTime().minute);
 				timeEntry.setCurrentHour(thisTask.getAlarmTime().hour);
@@ -214,11 +214,11 @@ public class TaskActivity extends Activity {
 		EditText taskNameEntry = (EditText) dialog.findViewById(R.id.taskNameEntry);
 		RatingBar ratingEntry = (RatingBar) dialog.findViewById(R.id.ratingEntry);
 		EditText notesEntry = (EditText) dialog.findViewById(R.id.notesEntry);
-		EditText postcodeEntry = (EditText) dialog.findViewById(R.id.postcodeEntry);
+		//EditText postcodeEntry = (EditText) dialog.findViewById(R.id.postcodeEntry);
 		TimePicker timeEntry = (TimePicker) dialog.findViewById(R.id.timeEntry);
 
 		// Create the new task
-		Task task = new Task(taskNameEntry.getText().toString(), notesEntry.getText().toString(), postcodeEntry.getText().toString(),
+		Task task = new Task(taskNameEntry.getText().toString(), notesEntry.getText().toString(), ""/*postcodeEntry.getText().toString()*/,
 				(int) ratingEntry.getRating());
 
 		Calendar c = Calendar.getInstance();
