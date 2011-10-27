b7f6dc79d2c2e6f8d593785a4ad2cdb8a0f3d10c
Harry Cutts <eternal.linux@gmail.com>
2011-10-27 16:47:41 +0100

    Fix for issue "POI on tap description sometimes not working" on issue tracker.

--- src/uk/org/todome/MapViewActivity.java
+++ src/uk/org/todome/MapViewActivity.java
@@ -21,6 +21,7 @@
  */
 package uk.org.todome;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -164,14 +165,14 @@ public class MapViewActivity extends MapActivity {
 			PointOfInterest poi = releventPOIsIter.next();
 			Log.i("MapViewActivity", "Found relevent POI: " + poi.getLatitudeE6() + " " + poi.getLongitudeE6());
 
-			String types = "";
+			ArrayList<String> types = new ArrayList<String>();
 			for (Iterator<String> typesIter = poi.getLocationTypes().iterator(); typesIter.hasNext();) {
 				String type = typesIter.next();
 				Log.i("MapViewActivity", "Found type " + type);
-				types = types + " " + type;
+				types.add(type);
 			}
 
-			String name = ToDoMeActivity.keywords.getDescriptionForType(types.split(" ")[0]);
+			String name = ToDoMeActivity.keywords.getDescriptionForType(types.get(0));
 
 			if (poi.getClosingTimes() != null) {
 				itemizedOverlay.addOverlay(new OverlayItem(poi.toGeoPoint(), name, poi.getOpeningTimes()[getDayOfWeek()] + " - "
